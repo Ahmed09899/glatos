1d6444113215bbb71fcb4409bf62a0fe78adbd4d
chrisholbrook <chrisholbrook@users.noreply.github.com>
2026-02-10 21:43:14 +0000

    [GitHub Actions] Style code

--- tests/testthat/test-read_glatos_workbook.r
+++ tests/testthat/test-read_glatos_workbook.r
@@ -1,11 +1,9 @@
-
 # Check workbook version 1.3
 
 wb13_file <- system.file("extdata", "walleye_workbook.xlsm", package = "glatos")
 wb13 <- read_glatos_workbook(wb13_file)
 
 test_that("metadata element gives expected result; v1.3", {
-
   expect_type(wb13[["metadata"]], "list")
 
   # Check if expected and actual results are the same
@@ -13,7 +11,6 @@ test_that("metadata element gives expected result; v1.3", {
 })
 
 test_that("receivers element gives expected result; v1.3", {
-
   expect_s3_class(wb13[["receivers"]], "glatos_receivers")
   expect_s3_class(wb13[["receivers"]], "data.frame")
 
@@ -22,7 +19,6 @@ test_that("receivers element gives expected result; v1.3", {
 })
 
 test_that("animals gives expected result; v1.3", {
-
   expect_s3_class(wb13[["animals"]], "glatos_animals")
   expect_s3_class(wb13[["animals"]], "data.frame")
 
@@ -35,22 +31,20 @@ test_that("animals gives expected result; v1.3", {
 wb13a <- read_glatos_workbook(wb13_file, read_all = TRUE)
 
 test_that("receivers element gives expected result; v1.3 (read_all = TRUE)", {
-  
   expect_s3_class(wb13a[["receivers"]], "glatos_receivers")
   expect_s3_class(wb13a[["receivers"]], "data.frame")
-  
+
   # Check if project-specific columns are present
   expect_true("receiver" %in% names(wb13a[["receivers"]]))
 })
 
 test_that("animals gives expected result; v1.3 (read_all = TRUE)", {
-  
   expect_s3_class(wb13a[["animals"]], "glatos_animals")
   expect_s3_class(wb13a[["animals"]], "data.frame")
-  
+
   # Check if project-specific columns are present
-  expect_true(all(c("release_year", "release_month", "release_doy") %in% 
-                names(wb13a[["animals"]])))
+  expect_true(all(c("release_year", "release_month", "release_doy") %in%
+    names(wb13a[["animals"]])))
 })
 
 
@@ -60,9 +54,8 @@ wb14_file <- system.file("extdata", "walleye_workbook.xlsx", package = "glatos")
 wb14 <- read_glatos_workbook(wb14_file)
 
 test_that("metadata element gives expected result; v1.4", {
-
   expect_type(wb14[["metadata"]], "list")
-  
+
   # Check if expected and actual results are the same
   # skip elements 4-6 (source_file, wb_version, created) because
   # the data object is format v.1.3.
@@ -73,19 +66,17 @@ test_that("metadata element gives expected result; v1.4", {
 })
 
 test_that("receivers element gives expected result; v1.4", {
-
   expect_s3_class(wb14[["receivers"]], "glatos_receivers")
   expect_s3_class(wb14[["receivers"]], "data.frame")
-  
+
   # Check if expected and actual results are the same
   expect_equal(wb14[["receivers"]], walleye_workbook[["receivers"]])
 })
 
 test_that("animals gives expected result; v1.4", {
-
-    expect_s3_class(wb14[["animals"]], "glatos_animals")
+  expect_s3_class(wb14[["animals"]], "glatos_animals")
   expect_s3_class(wb14[["animals"]], "data.frame")
-  
+
   # Check if expected and actual results are the same
   expect_equal(wb14[["animals"]], walleye_workbook[["animals"]])
 })
@@ -95,20 +86,18 @@ test_that("animals gives expected result; v1.4", {
 wb14a <- read_glatos_workbook(wb14_file, read_all = TRUE)
 
 test_that("receivers element gives expected result; v1.4 (read_all = TRUE)", {
-  
   expect_s3_class(wb14a[["receivers"]], "glatos_receivers")
   expect_s3_class(wb14a[["receivers"]], "data.frame")
-  
+
   # Check if project-specific columns are present
   expect_true("receiver" %in% names(wb14a[["receivers"]]))
 })
 
 test_that("animals gives expected result; v1.4 (read_all = TRUE)", {
-  
   expect_s3_class(wb14a[["animals"]], "glatos_animals")
   expect_s3_class(wb14a[["animals"]], "data.frame")
-  
+
   # Check if project-specific columns are present
-  expect_true(all(c("release_year", "release_month", "release_doy") %in% 
-                    names(wb14a[["animals"]])))
+  expect_true(all(c("release_year", "release_month", "release_doy") %in%
+    names(wb14a[["animals"]])))
 })
